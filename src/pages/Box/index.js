0d77003d0413fc7ccacdf1c8595350db679964ae
Paulo Henrique Freitas <paulo-henriquee@hotmail.com>
2019-04-13 22:13:04 -0300

    Ajuste URL backend

--- src/pages/Box/index.js
+++ src/pages/Box/index.js
@@ -26,7 +26,7 @@ export default class Box extends Component {
 
     subscribeToNewFiles = () => {
         const box = this.props.match.params.id;
-        const io = socket('https://phbox.herokuapp.com');
+        const io = socket('https://phbox-backend1.herokuapp.com');
 
         io.emit('connectRoom', box);
 
